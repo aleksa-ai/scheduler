2b8666708ff242268962161ffd5e01edca483270
Aleksa Gavrilovic <lks_g@live.ca>
2021-01-25 21:23:43 -0500

    TDD: Adding student name input valid. or to show an error (passing all tests for now) - TBD what happens to do with unselected interviewer (and possibly deleting?)

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,11 +7,22 @@ import Button from "../Button"
 export default function Form(props){
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
   const reset = () => {
     props.onCancel();
     setName("");
     setInterviewer(null);
   }
+
+  function validate() {
+    if (name === '') {
+      setError('Student name cannot be blank');
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -26,6 +37,7 @@ export default function Form(props){
             onChange={(event) => setName(event.target.value)}          
           />
         </form>
+        <section className='appointment__validation'>{error}</section>
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer} 
@@ -34,7 +46,7 @@ export default function Form(props){
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={reset}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
         </section>
       </section>
     </main>
