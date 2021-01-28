d42fbca21f454308092cf5fe42fa0394b6d8819b
Aleksa Gavrilovic <lks_g@live.ca>
2021-01-27 23:53:20 -0500

    Implement test to find list item, click & checks it for correct background colour (Intro)

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,13 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    
+    cy.contains('[data-testid=day]', 'Tuesday')
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)")
+  });
+
 });
